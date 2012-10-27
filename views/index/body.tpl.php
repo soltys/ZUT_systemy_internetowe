5356972172b8adfef702665dd474d85c9060756a
Paweł Sołtysiak <soltys@uznam.org>
2012-10-28 01:50:05 +0200

    prepare welcome message for logged user

--- views/index/body.tpl.php
+++ views/index/body.tpl.php
@@ -1,4 +1,10 @@
 <?php
-
+require_once ABSPATH . 'common/Authentication.php';
+$auth = Authentication::getInstance();
 ?>
 <h1>To jest strona główna</h1>
+<?php
+if ($auth->isUserLoggedIn()) {
+    echo "<p>Witaj ... </p>";
+}
+?>
